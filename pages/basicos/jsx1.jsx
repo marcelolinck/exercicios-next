132ec8f48da44d420a00f545a259c6d2ba3da5bb
marcelolinck <malveslinck@gmail.com>
2023-07-23 12:17:41 -0400

    Trabalhando com arquivos jsx

---
+++ pages/basicos/jsx1.jsx
@@ -0,0 +1,13 @@
+///JSX onde se mistura o html em um codigo javascript. O que importa é o nome do arquivo e nao o nome da funcao para chamar a url
+
+export default function segundo(){
+    //Assim nao interpreta como H1 mesmo
+    //return "<h1>segundo</h1>"
+    //Quando se tem multiplas linhas se usa o parenteses
+
+    return (
+        <div>
+            <h1>JSX #01</h1>
+        </div>
+        )
+}
