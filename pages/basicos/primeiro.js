4b77755e10df88d46bb5e9e1a00577b4b93a4ae0
marcelolinck <malveslinck@gmail.com>
2023-07-23 09:54:52 -0400

    Aula inicial de Next

---
+++ pages/basicos/primeiro.js
@@ -0,0 +1,6 @@
+//Quando criar o arquivo dentro da pagina pages, o next interpreta como sendo o caminho para ser acessado via url, nesse exemplo ele criou a url /primeiro
+
+export default function primeiro() {
+    return "Primeiro V2"
+}
+
