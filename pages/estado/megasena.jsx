3b5d660e9f1ce0d0272fd15d97d1bb95ec74667d
marcelolinck <malveslinck@gmail.com>
2023-08-09 23:21:26 -0400

    ajustes para inicio aula 47

--- pages/estado/megasena.jsx
+++ pages/estado/megasena.jsx
@@ -1,9 +1,7 @@
-import {useState} from "react"
-import {mega} from "../../functions/mega"
+
 
 export default function megasena(){
-    const [numeros, setNumeros] = useState(mega())
-
+  
     return(
         <div>
             <h1>Mega Sena</h1>
