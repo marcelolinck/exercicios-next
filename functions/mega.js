a9d9d7d1149182c17157d70616e6fa0a4e9b1f79
marcelolinck <malveslinck@gmail.com>
2023-08-12 08:18:41 -0400

    Finalizado o exercicio MegaSena.

--- functions/mega.js
+++ functions/mega.js
@@ -1,22 +1,27 @@
-export function mega(qtde = 6, numeros = []){
-    if (qtde < 6 || qtde > 60) {
-        throw "Quantidade inválida!"
+export function mega(quantidade = 6, numeros = []) {
+   //Dessa forma estamos convertendo uma string para 
+   let qtde = +quantidade
+    
+   if (qtde < 6 || qtde > 60) {
+      throw "Quantidade inválida!"
     }
+    // Caso base: Se a quantidade desejada de números foi atingida, ordenar e retornar a lista
     if (numeros.length === qtde) {
-        //O metodo sort faz a ordenação dos dados, só que deve-se passar os demais parametros para que funcione
-        return numeros.sort((n1, n2)=> n1 - n2)
+      return numeros.sort((n1, n2) => n1 - n2)
     }
-
-    //Dessa forma cria um numero aleatorio de 1 a 60 com o +1 do lado, pois os numeros em si são de 0 até o limite, nesse caso até 59
+    
+    // Gera um número aleatório de 1 a 60
     const numeroAleatorio = parseInt(Math.random() * 60) + 1
     
-    //Aqui verifica se o numero aleatorio está incluido na lista
+    // Verifica se o número aleatório já foi gerado
     if (!numeros.includes(numeroAleatorio)) {
-        //O ...numeros faz uma copia da lista numeros para uma nova lista. Com isso ele vai gerar novos numeros até chegar o limite
-        return mega(qtde, [...numeros, numeroAleatorio])
-    }else{
-        return mega(qtde,numeros)
+      // Adiciona o número aleatório à lista e continua gerando
+      return mega(qtde, [...numeros, numeroAleatorio])
     }
-}
-
-console.log(mega(61))
+    
+    // Número aleatório já foi gerado, tentar novamente
+    else {
+        return mega(qtde, numeros)
+    }
+  }
+  
