12cbc31b121e9f72f6f52debc10da1009d19938d
marcelolinck <malveslinck@gmail.com>
2023-08-12 09:03:17 -0400

    Finalizado componente megaSena

---
+++ components/Contador.jsx
@@ -0,0 +1,14 @@
+import { Component} from "react";
+
+export default class Contador extends Component{
+
+    // Com o metodo render, consigo rendenizar uma classe na tela
+
+    render(){
+        return(
+            <div>
+                <h1>Teste Classe</h1>
+            </div>
+        )
+    }
+}
--- pages/estado/megasena.jsx
+++ pages/estado/megasena.jsx
@@ -1,10 +1,44 @@
+import { useState, useEffect } from "react"
+import NumeroDisplay from "../../components/NumeroDisplay"
+import { mega } from "../../functions/mega"
 
 
-export default function megasena(){
+
+export default function megasena() {
+
+  const [qtde, setQtde] = useState(6);
+
+  const [numeros, setNumeros] = useState([])
+
+  useEffect(() => {
+    setNumeros(mega())
+  }, [])
   
-    return(
-        <div>
-            <h1>Mega Sena</h1>
-        </div>
+  function rendenizarNumeros() {
+    return numeros.map(
+        n => <NumeroDisplay key={n} numero={n} />
     )
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Mega Sena</h1>
+      <div style={{ display: "flex",
+    flexWrap: "wrap" }} >
+        {rendenizarNumeros()}
+      </div>
+      <div>
+      <input type="number" min={6} max={20} onChange={ev => setQtde(parseInt(+ev.target.value))} value={qtde}/>
+
+        {/* Dessa forma estamos passando uma funcao anonima quando clicar o novo numero. Se deixar sem a funcao, ele vai carregar o numero quando carregar a pagina */}
+        <button onClick={() => setNumeros(mega(qtde))}>Gerar Aposta</button>
+      </div>
+    </div>
+  );
 }
